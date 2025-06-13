c210f31e33f606fde21b1eed0e682b273d71a2c7
Sajid <sajidali2444@gmail.com>
2025-06-13 22:51:46 +0500

    deploy test 4

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "@react-router/dev/config"
+import { vercelPreset } from "@vercel/react-router/vite"
 
 export default {
 	future: {
@@ -6,4 +7,5 @@ export default {
 		unstable_splitRouteModules: true,
 		unstable_optimizeDeps: true,
 	},
+	presets: [vercelPreset()],
 } satisfies Config
