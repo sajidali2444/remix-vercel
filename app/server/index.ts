1ee08788e8da787a1479b639cdd5123fb429e486
Sajid <sajidali2444@gmail.com>
2025-06-13 22:39:48 +0500

    deploy test 4

--- app/server/index.ts
+++ app/server/index.ts
@@ -1,4 +1,3 @@
-import { serveStatic } from "@hono/node-server/serve-static"
 import { createHonoServer } from "react-router-hono-server/node"
 import { i18next } from "remix-hono/i18next"
 import i18nextOpts from "../localization/i18n.server"
@@ -7,9 +6,6 @@ import { getLoadContext } from "./context"
 export default await createHonoServer({
 	configure(server) {
 		server.use("*", i18next(i18nextOpts))
-		// Serve static files
-		server.use("/assets/*", serveStatic({ root: "./build/client" }))
-		server.use("/favicon.ico", serveStatic({ path: "./build/client/favicon.ico" }))
 	},
 	defaultLogger: false,
 	getLoadContext,
